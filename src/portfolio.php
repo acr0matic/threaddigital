fd18ee612fbecdc48c4a091a686b54e8c4873f6b
Nikita <main.acr0matic@gmail.com>
2021-02-17 19:14:57 +0300

    feat: верстка главного экрана страницы кейса

--- src/portfolio.php
+++ src/portfolio.php
@@ -1,2 +1,31 @@
 <?php include_once 'partials/header.php' ?>
+
+<main>
+  <section id="hero">
+    <div class="container">
+      <div class="hero">
+        <div class="row">
+          <div class="col-6">
+            <h1 class="hero__title section__title mb-4">
+              tatarka.store
+            </h1>
+
+            <p class="hero__description section__description">
+              Дизайн и ведение аккаунта
+            </p>
+
+            <button class="hero__button hero__button--portfolio button button-secondary" style="background-color: #D00E9A; color: #ffffff">Вперед!</button>
+          </div>
+          <!-- /.col-6s -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.hero -->
+    </div>
+    <!-- /.container -->
+
+    <img class="hero__image" src="img/page/portfolio/hero/background.png" alt="">
+  </section>
+</main>
+
 <?php include_once 'partials/footer.php' ?>
