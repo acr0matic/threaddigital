9be5286db40f5f150553dd1c8b60820f3512e37e
Nikita <main.acr0matic@gmail.com>
2021-02-17 18:29:10 +0300

    feat: верстка модального окна

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -27,3 +27,25 @@ const feedbackSlider = new Swiper('.feedback-slider', {
 })
 
 scrollTo();
+
+const modalParams = {
+  awaitCloseAnimation: true,
+  disableScroll: true,
+  disableFocus: true,
+};
+
+MicroModal.init(modalParams);
+AOS.init({ once: true, offset: 240 });
+
+const callbackButton = document.getElementById('callback-button');
+const callbackModal = document.getElementById('modal-callback');
+
+callbackButton.addEventListener('click', () => {
+  const animated = callbackModal.querySelectorAll('.aos-animate');
+
+  // Анимация
+  animated.forEach(item => {
+    item.classList.remove('aos-animate');
+    setTimeout(() => item.classList.add('aos-animate'), 100);
+  });
+});
