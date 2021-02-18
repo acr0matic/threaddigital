e305598405c3c9217351fb7d82bd61b4146b18dc
Nikita <main.acr0matic@gmail.com>
2021-02-18 12:42:32 +0300

    feat: верстка описания

---
+++ src/case.php
@@ -0,0 +1,52 @@
+<?php include_once 'partials/header.php' ?>
+
+<main id="case">
+  <section id="hero">
+    <div class="container">
+      <div class="hero">
+        <div class="row">
+          <div class="col-6">
+            <h1 class="hero__title section__title mb-4">
+              tatarka.store
+            </h1>
+
+            <p class="hero__description section__description">
+              Дизайн и ведение аккаунта
+            </p>
+
+            <button class="hero__button hero__button--portfolio button button-secondary" style="background-color: #D00E9A; color: #ffffff">Вперед!</button>
+          </div>
+          <!-- /.col-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.hero -->
+    </div>
+    <!-- /.container -->
+
+    <img class="hero__image" src="img/page/portfolio/hero/background.png" alt="">
+  </section>
+
+  <section id="description" class="section">
+    <div class="container">
+      <div class="description">
+        <div class="row">
+          <div class="col-5">
+            <p style="--border-color: #D00E9A;" class="description__text">Tatarka.Store - проект российской поп-исполнительницы певицы, музыкальная авторка, участницы лейбла Little Big Family. Tatarka.Store - это официальный интернет-магазин мерча, представление которого в соцсетях является неотъемлемой частью продаж и формирования образа потребления. </p>
+          </div>
+          <!-- /.col-5 -->
+
+          <div class="col-6 offset-1">
+            <img class="description__image" src="img/page/portfolio/description/logo.png" alt="">
+          </div>
+          <!-- /.col-7 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.description -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once 'partials/footer.php' ?>
