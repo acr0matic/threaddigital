64c23cf3677e56762e1f95f28df1b820b62cfbee
Nikita <main.acr0matic@gmail.com>
2021-02-21 16:55:18 +0300

    feat: верстка "другие кейсы"

---
+++ src/other.php
@@ -0,0 +1,70 @@
+<?php include_once 'partials/header.php' ?>
+
+<main>
+  <section id="other" class="section">
+    <div class="container">
+      <h2 class="section__title">Все кейсы</h2>
+
+      <div class="other">
+        <div class="row">
+          <div class="col-12 col-md-6 mb-5">
+            <div class="other__item">
+              <img src="img/page/other/1.jpg" alt="" class="other__image">
+              <div class="other__content">
+                <h3 class="other__title">Кейс нейм</h3>
+                <p class="other__description">Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. </p>
+              </div>
+              <!-- /.other__content -->
+            </div>
+            <!-- /.other__item -->
+          </div>
+          <!-- /.col-6 -->
+
+          <div class="col-12 col-md-6 mb-5">
+            <div class="other__item">
+              <img src="img/page/other/1.jpg" alt="" class="other__image">
+              <div class="other__content">
+                <h3 class="other__title">Кейс нейм</h3>
+                <p class="other__description">Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. </p>
+              </div>
+              <!-- /.other__content -->
+            </div>
+            <!-- /.other__item -->
+          </div>
+          <!-- /.col-6 -->
+
+          <div class="col-12 col-md-6 mb-5">
+            <div class="other__item">
+              <img src="img/page/other/1.jpg" alt="" class="other__image">
+              <div class="other__content">
+                <h3 class="other__title">Кейс нейм</h3>
+                <p class="other__description">Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. </p>
+              </div>
+              <!-- /.other__content -->
+            </div>
+            <!-- /.other__item -->
+          </div>
+          <!-- /.col-6 -->
+
+          <div class="col-12 col-md-6 mb-5">
+            <div class="other__item">
+              <img src="img/page/other/1.jpg" alt="" class="other__image">
+              <div class="other__content">
+                <h3 class="other__title">Кейс нейм</h3>
+                <p class="other__description">Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. Описание кейса. </p>
+              </div>
+              <!-- /.other__content -->
+            </div>
+            <!-- /.other__item -->
+          </div>
+          <!-- /.col-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.other -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once 'partials/footer.php' ?>
