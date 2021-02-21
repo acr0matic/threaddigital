1003b3c338b8718aa10a8ce42396ffc97c957c34
Nikita <main.acr0matic@gmail.com>
2021-02-21 04:16:19 +0300

    fix: убрано копирование с мобиольной версии

--- src/scripts/copy.js
+++ src/scripts/copy.js
@@ -8,35 +8,37 @@ function Copy(container, copyBlock, link) {
 
   element.addEventListener('click', (e) => {
     if (window.matchMedia("(max-width: 996px)").matches) return;
-    else e.preventDefault();
-  });
+    else {
+      e.preventDefault();
 
-  copyBlock.addEventListener('click', () => {
-    count++;
+      copyBlock.addEventListener('click', () => {
+        count++;
 
-    if (linkRef) text = linkRef.innerHTML;
-    else text = element.innerHTML;
+        if (linkRef) text = linkRef.innerHTML;
+        else text = element.innerHTML;
 
-    if (count === 2) tooltip.innerHTML = 'Еще раз';
-    if (count === 3) {
-      tooltip.innerHTML = 'Точно скопировано';
-      tooltip.classList.add('tooltip--gradient');
-    }
+        if (count === 2) tooltip.innerHTML = 'Еще раз';
+        if (count === 3) {
+          tooltip.innerHTML = 'Точно скопировано';
+          tooltip.classList.add('tooltip--gradient');
+        }
 
-    tooltip.classList.add('tooltip--visible');
+        tooltip.classList.add('tooltip--visible');
 
-    new ClipboardJS(container, { text: () => text });
+        new ClipboardJS(container, { text: () => text });
 
-    window.clearTimeout(tooltipTimer);
-    tooltipTimer = window.setTimeout(() => {
-      tooltip.classList.remove("tooltip--visible");
-      setTimeout(() => Reset(), 100);
-    }, 1000);
+        window.clearTimeout(tooltipTimer);
+        tooltipTimer = window.setTimeout(() => {
+          tooltip.classList.remove("tooltip--visible");
+          setTimeout(() => Reset(), 100);
+        }, 1000);
 
-    function Reset() {
-      tooltip.classList.remove('tooltip--gradient');
-      tooltip.innerHTML = 'Скопировано';
-      count = 0;
+        function Reset() {
+          tooltip.classList.remove('tooltip--gradient');
+          tooltip.innerHTML = 'Скопировано';
+          count = 0;
+        }
+      });
     }
   });
 }
